0bf8d4a74aee0f054b1c4eca0fd8e68c297af380
Kamrul Hasan <kamrulsdev@gmail.com>
2022-03-19 22:24:09 +0600

    How states works, asynchronous, console state

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import Device from './Components/Device/Device';
+import Watch from './Components/Watch/Watch';
 
 function App() {
   return (
     <div className="App">
       <Device name="iPhone" price="75000"></Device>
+      <Watch></Watch>
     </div>
   );
 }
